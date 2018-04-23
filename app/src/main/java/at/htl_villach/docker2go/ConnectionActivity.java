c5fc7d6c05857d64b00aef2d2d9070f173e001e9
David Kröll <david.kroell@outlook.com>
2018-04-23 13:43:53 +0200

    initial project setup

---
+++ app/src/main/java/at/htl_villach/docker2go/ConnectionActivity.java
@@ -0,0 +1,13 @@
+package at.htl_villach.docker2go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+    }
+}
