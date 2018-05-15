bbe909eb0e219a1d36311ae7eb51b4fd8ecb8a0f
David Kröll <david.kroell@outlook.com>
2018-05-15 12:16:55 +0200

    add documentation

--- app/src/main/java/at/htl_villach/docker2go/AsyncTaskCommandExecutor.java
+++ app/src/main/java/at/htl_villach/docker2go/AsyncTaskCommandExecutor.java
@@ -15,15 +15,14 @@ import java.util.Properties;
 
 
 // this is the class which runs the long lasting operation
-// first param type: backgroundParams
-// second param type: updateValues
-// third param type: resultValue
-public class AsyncTaskCommandExecutor extends AsyncTask<Command, String, String> {
+// first param type: backgroundParams, the command to be executed
+// second param type: updateValues, with this Object, the listener is called
+// third param type: resultValue, with this Object, the listener is called
+public class AsyncTaskCommandExecutor extends AsyncTask<Command, Object, Object> {
 
     private Connection connection;
     private Connection.onCommandStatusChangeListener listener;
 
-    private JSch jsch;
     private Session jschSession;
 
     public AsyncTaskCommandExecutor(Connection.onCommandStatusChangeListener listener, Connection connection) {
@@ -35,9 +34,9 @@ public class AsyncTaskCommandExecutor extends AsyncTask<Command, String, String>
     protected void onPreExecute() {
         // connect to server
         try{
-            this.jsch = new JSch();
+            JSch jsch = new JSch();
 
-            this.jschSession = this.jsch.getSession(
+            this.jschSession = jsch.getSession(
                     this.connection.getUsername(),
                     this.connection.getHostname(),
                     this.connection.getSshPort());
@@ -45,6 +44,7 @@ public class AsyncTaskCommandExecutor extends AsyncTask<Command, String, String>
             this.jschSession.setPassword(this.connection.getPassword());
 
             // Avoid asking for key confirmation
+            // TODO: add host key checking with users intervention
             Properties prop = new Properties();
             prop.put("StrictHostKeyChecking", "no");
             this.jschSession.setConfig(prop);
@@ -58,23 +58,33 @@ public class AsyncTaskCommandExecutor extends AsyncTask<Command, String, String>
         }
     }
 
+    // executed every command on remote machine in separated channels
+    // still using same channel
     @Override
     protected String doInBackground(Command... backgroundParams) {
         try {
+            // connect to remote machine
             this.jschSession.connect(this.connection.getConnectionTimeout());
 
             // Execute single command and use onProgressUpdate for output
-            for (int i = 0; i < backgroundParams.length; i++){
+            for (Command command : backgroundParams) {
                 ChannelExec channelssh = (ChannelExec) this.jschSession.openChannel("exec");
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 channelssh.setOutputStream(baos);
 
-                channelssh.setCommand(backgroundParams[i].parseString());
+                // use Commands parseString method to get the actual command
+                // in String representation e.g.: "curl localhost"
+                channelssh.setCommand(command.parseString());
                 channelssh.connect();
 
-                while (channelssh.getExitStatus() != backgroundParams[i].getExpectedExitCode()) {
-                    Thread.sleep(backgroundParams[i].getRefreshTimeOut());
+                // wait for the command until it exits with the expected exit code
+                // TODO: what if it exits with another exit code?
+                int refreshTimeout = command.getRefreshTimeOut();
+                while (channelssh.getExitStatus() != command.getExpectedExitCode()) {
+                    Thread.sleep(refreshTimeout);
                 }
+
+                // calls method onProgressUpdate
                 publishProgress(baos.toString("utf8"));
                 channelssh.disconnect();
             }
@@ -88,21 +98,20 @@ public class AsyncTaskCommandExecutor extends AsyncTask<Command, String, String>
         }
     }
 
+    // called by doInBackground's publishProgress() method,
+    // this will call the listener's method onCommandFinished() with each Object separately
     @Override
-    protected void onProgressUpdate(String... updateValues) {
-        // Things to be done while execution of long running operation is in progress
-        // This method is triggered by calling the method publishProgress()
-        // The updateValues are the params of the method publishProgress()
-
-        for (int i = 0; i < updateValues.length; i++) {
-            listener.onCommandFinished(updateValues[i]);
+    protected void onProgressUpdate(Object... updateValues) {
+        for (Object updateValue : updateValues) {
+            listener.onCommandFinished(updateValue);
         }
     }
 
+    // called by return from doInBackground
+    // return value should be "finished" if everything went correct.
+    // if something went wrong, the String returned is the message of the Exception
     @Override
-    protected void onPostExecute(String resultValue) {
-        // Things to be done after the long running operation
-        // for example showing the result value
+    protected void onPostExecute(Object resultValue) {
         listener.onAllCommandsFinished(resultValue);
     }
 }
